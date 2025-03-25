fe656d02512b7ae2265c683325f997bdda41bf31
Kelvyn Ferreira de Melo <kelvyn.melo@itaipuparquetec.org.br>
2025-03-25 15:14:24 -0300

    Nova linha crida no código

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def imprimi_pares(n):
+def imprime_pares(n):
     for i in range(0, n):
         if i % 2 == 0:
             print(i)
 
 if __name__ == '__main__':
-    imprimi_pares(10)
-    
+    imprime_pares(10)
+    # Nova linha
